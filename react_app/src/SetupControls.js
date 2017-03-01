ac4ca6d4210f434484144731402c3f81623b3880
Jimbo <marquardtjames@gmail.com>
2017-03-01 12:31:16 -0600

    setupControls use onBlur and defaultValue instead of onChange and value, to prevent each keypress from changing values. want to wait until whole value is typed before processing it

--- react_app/src/SetupControls.js
+++ react_app/src/SetupControls.js
@@ -5,18 +5,17 @@ class SetupControls extends Component{
 	render(){
 		return(
 			<div className="setup-controls">
-				gamepoint: {this.props.state.gamePoint}
 				<label className="label">Game Point</label>
 				<p className="control">
-					<input className="input" type="number" value={this.props.state.gamePoint} onChange={this.props.handlers.gamePoint}></input>
+					<input className="input" type="number" defaultValue={this.props.state.gamePoint} onBlur={this.props.handlers.gamePoint}></input>
 				</p>
 				<label className="label">Twos Worth</label>
 				<p className="control">
-					<input className="input" type="number" value={this.props.state.pointValues.two} onChange={this.props.handlers.twoValue}></input>
+					<input className="input" type="number" defaultValue={this.props.state.pointValues.two} onBlur={this.props.handlers.twoValue}></input>
 				</p>
 				<label className="label">Threes Worth</label>
 				<p className="control">
-					<input className="input" type="number" value={this.props.state.pointValues.three} onChange={this.props.handlers.threeValue}></input>
+					<input className="input" type="number" defaultValue={this.props.state.pointValues.three} onBlur={this.props.handlers.threeValue}></input>
 				</p>
 				<p className="control">
 					<button className="button" onClick={this.props.handlers.reset}>Reset Game</button>
