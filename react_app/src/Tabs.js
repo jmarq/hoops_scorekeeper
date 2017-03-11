dbde7ef12976135ebc48ce015cea45520fc120d3
Jimbo <marquardtjames@gmail.com>
2017-03-11 14:52:45 -0600

    new tab for play by play, play by play css, padding for team stats

--- react_app/src/Tabs.js
+++ react_app/src/Tabs.js
@@ -13,6 +13,7 @@ class Tabs extends Component{
 			  	<li className={this.tabClass("setup")}><a data-tab="setup" onClick={this.props.handler}>Setup</a></li>
 			    <li className={this.tabClass("score")} ><a data-tab="score" onClick={this.props.handler}>Score</a></li>
 			    <li className={this.tabClass("stats")}><a data-tab="stats" onClick={this.props.handler}>Stats</a></li>
+			    <li className={this.tabClass("plays")}><a data-tab="plays" onClick={this.props.handler}>Plays</a></li>
 			  </ul>
 			</div>
 		);
