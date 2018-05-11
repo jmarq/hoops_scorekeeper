57a9bd4e66844d898d32ba84f5ff867634bb0deb
Jimbo <marquardtjames@gmail.com>
2018-05-10 22:29:27 -0500

    test updated

--- react_app/src/components/PlayByPlay/PlayByPlay.test.js
+++ react_app/src/components/PlayByPlay/PlayByPlay.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {mount} from 'enzyme';
 import PlayByPlay from './PlayByPlay';
 
-const testProps = {
+let testProps = {
   teamNames: ['Team 1', 'Team 2'],
   pointValues: {two: 1, three: 3},
   plays: [
@@ -27,4 +27,15 @@ it('renders correct scoring play', () => {
   // console.log(first_play);
   // console.log(scoreboardTeam1);
   expect(firstPlay).toEqual('Team 1 - score 1');
+});
+
+it('renders message when there are no plays yet', () => {
+  let emptyPlaysProps = {...testProps};
+  emptyPlaysProps.plays = [];
+  const wrapper = mount(
+	  <PlayByPlay {...emptyPlaysProps}/>
+	);	
+	let plays = wrapper.find('.play');
+	let firstPlay = plays.at(0).text();
+	expect(firstPlay).toEqual("No plays yet");
 });
